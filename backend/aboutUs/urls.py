fd0b6f96c14aa5b227aba8eb1b6f38fa4a41fad2
tlbkvs <tlbkvs@gmail.com>
2025-11-11 14:52:43 +0600

    .

--- backend/aboutUs/urls.py
+++ backend/aboutUs/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import AboutUsAPIView
 
 urlpatterns = [
-    path("pages/<slug:slug>/", AboutUsAPIView.as_view(), name="page-content"),
+    path("", AboutUsAPIView.as_view(), name="page-content"),
 ]
