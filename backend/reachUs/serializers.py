b3f533bc0be03ba8dd40999ba1c863c99d215b07
tlbkvs <tlbkvs@gmail.com>
2025-11-11 18:13:14 +0600

    .

--- backend/reachUs/serializers.py
+++ backend/reachUs/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from .models import ReachUsHeroSection, ReachUsSecondSection
+from home.serializers import AbsoluteImageUrlField
 
 class ReachUsHeroSectionSerializer(serializers.ModelSerializer):
+    image = AbsoluteImageUrlField()  # замените на реальное имя поля картинки, если отличается
+
     class Meta:
         model = ReachUsHeroSection
         fields = "__all__"
 
+
+# ✅ Reach Us Second Section
 class ReachUsSecondSectionSerializer(serializers.ModelSerializer):
+    image = AbsoluteImageUrlField()  # замените на реальное имя поля картинки, если отличается
+
     class Meta:
         model = ReachUsSecondSection
         fields = "__all__"
