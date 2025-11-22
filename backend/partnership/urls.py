6a979f518ad6aa8210f3073607b0d3f3f9e10697
tlbkvs <tlbkvs@gmail.com>
2025-11-22 15:24:27 +0600

    .

--- backend/partnership/urls.py
+++ backend/partnership/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import PartnershipRequestCreateView
 
 urlpatterns = [
-    path('partnership/', PartnershipRequestCreateView.as_view(), name='partnership_create'),
+    path('', PartnershipRequestCreateView.as_view(), name='partnership_create'),
 ]
